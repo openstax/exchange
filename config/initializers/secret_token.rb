60acdbde8375007ec68432e2dc8fc0b5e13ca389
JP Slavinsky <jpslav@gmail.com>
2013-07-22 16:38:19 -0700

    secret settings for token, duh

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Exchange::Application.config.secret_token = 'ed7592e5ab5aec106af2aee88b5466f1df3a625d6aaad7b83f4b28b5a098b796580e967ad4277dca6738a46436546f7c90b391ebd24b820c3224e589698954b8'
+Exchange::Application.config.secret_token = 
+  SECRET_SETTINGS[:secret_token] || 'ed7592e5ab5aec106af2aee88b5466f1df3a625d6aaad7b83f4b28b5a098b796580e967ad4277dca6738a46436546f7c90b391ebd24b820c3224e589698954b8'
