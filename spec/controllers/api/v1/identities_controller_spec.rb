3075b8d4548ab99d96983962826aaffb0c070c3b
JP Slavinsky <jpslav@gmail.com>
2014-05-31 13:50:40 -0700

    more cleanup

--- spec/controllers/api/v1/identities_controller_spec.rb
+++ spec/controllers/api/v1/identities_controller_spec.rb
@@ -1,33 +1,37 @@
-require "spec_helper"
+#
+# *** This deprecated file left as an example for the future Exchange developer
+#
 
-describe Api::V1::IdentitiesController, :type => :api, :version => :v1 do
+# require "spec_helper"
 
-  let(:user) { FactoryGirl.create(:user, :is_registered => true) }
-  let(:user_access_token) { FactoryGirl.create(:access_token,
-                                               :resource_owner_id => user.id) }
-  let(:app_access_token) { FactoryGirl.create(:access_token,
-                                              :resource_owner_id => nil) }
+# describe Api::V1::IdentitiesController, :type => :api, :version => :v1 do
 
-  it "should be creatable by an app with an access token" do
-    api_post :create, app_access_token
-    expect(response.status).to eq(201)
+#   let(:user) { FactoryGirl.create(:user, :is_registered => true) }
+#   let(:user_access_token) { FactoryGirl.create(:access_token,
+#                                                :resource_owner_id => user.id) }
+#   let(:app_access_token) { FactoryGirl.create(:access_token,
+#                                               :resource_owner_id => nil) }
 
-    expected_response = {
-      :identity => Identity.last.value
-    }.to_json
-    expect(response.body).to eq(expected_response)
-  end
+#   it "should be creatable by an app with an access token" do
+#     api_post :create, app_access_token
+#     expect(response.status).to eq(201)
 
-  it "should not be creatable by a user with an access token" do
-    c = Identity.count
-    expect{api_post :create, user_access_token}.to raise_error(Lev::SecurityTransgression)
-    expect(Identity.count).to eq(c)
-  end
+#     expected_response = {
+#       :identity => Identity.last.value
+#     }.to_json
+#     expect(response.body).to eq(expected_response)
+#   end
 
-  it "should not be creatable without an access token" do
-    c = Identity.count
-    expect{api_post :create, nil}.to raise_error(Lev::SecurityTransgression)
-    expect(Identity.count).to eq(c)
-  end
+#   it "should not be creatable by a user with an access token" do
+#     c = Identity.count
+#     expect{api_post :create, user_access_token}.to raise_error(Lev::SecurityTransgression)
+#     expect(Identity.count).to eq(c)
+#   end
 
-end
+#   it "should not be creatable without an access token" do
+#     c = Identity.count
+#     expect{api_post :create, nil}.to raise_error(Lev::SecurityTransgression)
+#     expect(Identity.count).to eq(c)
+#   end
+
+# end
