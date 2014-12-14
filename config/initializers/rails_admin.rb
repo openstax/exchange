caa51b8cf4560629af99e1016567dd27a7844a73
Dante Soares <dante.m.soares@rice.edu>
2014-12-14 12:52:35 -0600

    Added authentication to rails_admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,20 +1,13 @@
 RailsAdmin.config do |config|
 
-  ### Popular gems integration
-
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
-
-  ## == Cancan ==
-  # config.authorize_with :cancan
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+
+  config.authenticate_with do
+    raise(SecurityTransgression) \
+      if Rails.env.production? && current_administrator.nil?
+  end
+
+  config.current_user_method(&:current_account)
 
   config.included_models = ["OpenStax::Accounts::Account",
                             "Doorkeeper::Application",
