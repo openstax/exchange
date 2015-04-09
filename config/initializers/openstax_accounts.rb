4727416292651862bdadf51dc82bfda6050eebee
Dante Soares <dms3@rice.edu>
2015-04-09 13:41:24 -0500

    Allow secrets.yml to specify the accounts url and stubbing enable

--- config/initializers/openstax_accounts.rb
+++ config/initializers/openstax_accounts.rb
@@ -1,8 +1,12 @@
 OpenStax::Accounts.configure do |config|
-  config.openstax_accounts_url = 'http://localhost:2999/' if !Rails.env.production?
-  accounts_secrets = Rails.application.secrets['openstax']['accounts']
-  config.openstax_application_id = accounts_secrets['client_id']
-  config.openstax_application_secret = accounts_secrets['secret']
+  secrets = Rails.application.secrets['openstax']['accounts']
+
+  # By default, stub unless in the production environment
+  stub = secrets['stub'].nil? ? !Rails.env.production? : secrets['stub']
+
+  config.openstax_application_id = secrets['client_id']
+  config.openstax_application_secret = secrets['secret']
+  config.openstax_accounts_url = secrets['url']
+  config.enable_stubbing = stub
   config.logout_via = :delete
-  config.enable_stubbing = true
 end
