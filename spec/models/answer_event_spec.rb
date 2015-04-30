a42e318c7e2f3185632328f01ea8dac65a9dd4e4
Dante Soares <dms3@rice.edu>
2015-04-30 13:30:07 -0500

    Removed check for answer_type uniqueness in answer events

--- spec/models/answer_event_spec.rb
+++ spec/models/answer_event_spec.rb
@@ -12,28 +12,6 @@ RSpec.describe AnswerEvent, :type => :model do
       expect(answer_event.errors.messages).to eq(
         :answer_type => ["can't be blank"])
     end
-
-    it 'must be unique for each task' do
-      answer_event.save!
-
-      answer_event_2 = FactoryGirl.build(:answer_event,
-                                         task: answer_event.task)
-      expect(answer_event_2).not_to be_valid
-      expect(answer_event_2.errors.messages).to eq(
-        :answer_type => ["has already been taken"])
-
-      answer_event_2.answer_type = 'another-type'
-      answer_event_2.save!
-
-      answer_event_3 = FactoryGirl.build(:answer_event,
-                                         task: answer_event.task)
-      expect(answer_event_3).not_to be_valid
-      expect(answer_event_3.errors.messages).to eq(
-        :answer_type => ["has already been taken"])
-
-      answer_event_3.task = FactoryGirl.build(:task)
-      answer_event_3.save!
-    end
   end
 
 end
