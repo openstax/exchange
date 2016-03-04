976740ce312a37585492d1cb193b4852daecfb87
JP Slavinsky <jpslav@gmail.com>
2016-03-03 17:19:14 -0800

    Undid FindOrCreateTask lock

--- app/routines/find_or_create_task.rb
+++ app/routines/find_or_create_task.rb
@@ -8,18 +8,10 @@ class FindOrCreateTask
   # otherwise saves and returns the task
   def exec(task)
 
-    lock_name = self.class.name
-
-    lock_result = Task.with_advisory_lock_result(lock_name, DEFAULT_LOCK_TIMEOUT_SECS) do
-      outputs[:task] = Task.find_by(identifier: task.identifier,
-                                    resource: task.resource,
-                                    trial: task.trial) || task
-      outputs[:task].save unless outputs[:task].persisted?
-    end
-
-    if !lock_result.lock_was_acquired?
-      fatal_error(code: :lock_not_acquired, message: "Could not acquire '#{lock_name}' lock")
-    end
+    outputs[:task] = Task.find_by(identifier: task.identifier,
+                                  resource: task.resource,
+                                  trial: task.trial) || task
+    outputs[:task].save unless outputs[:task].persisted?
 
     transfer_errors_from(outputs[:task], {type: :verbatim}, true)
 
